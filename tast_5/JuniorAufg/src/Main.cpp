fd21b0923c41d82f02955f01c8937a1dd6c93ce2
Pascal-1609 <pascal2003.k@gmail.com>
2020-09-27 13:49:12 +0200

    Add files

--- tast_5/JuniorAufg/src/Main.cpp
+++ tast_5/JuniorAufg/src/Main.cpp
@@ -1,8 +1,24 @@
 #include <cstdlib>
 #include <iostream>
+#include <fstream>
+#include <string>
 #include <ctime>
 
+
 int main() {
+
+	std::ifstream file("Beispieldateien/spielstaerken1.txt");
+	if (!file.is_open())
+	{
+		std::cout << "Unable to open file!" << std::endl;
+		return -1;
+	}
+	std::string line;
+	while (std::getline(file, line))
+	{
+		std::cout << line << std::endl;
+	}
+	file.close();
 
 	// Add random number
 	std::srand(std::time(0)); //use current time as seed for random generator
