1a326affa8d29a7488f01be1e2e19ede3ed4ff4c
Christopher Besch <christopher.besch@gmx.de>
2020-11-09 17:20:05 +0100

    =

--- task_5/main.py
+++ task_5/main.py
@@ -2,6 +2,9 @@ import re
 
 
 class Student:
+    """
+    representing a single student with references to their wished packages
+    """
     def __init__(self, number, wishes):
         self.number = number
         # tuple with one package number per wish (1st wish, 2nd wish...)
@@ -12,9 +15,12 @@ class Student:
 
 
 class Package:
+    """
+    representing a single package with references to all the students wanting this package
+    """
     def __init__(self, number, wishers):
         self.number = number
-        # tuple with one list per wish (1st wish, 2nd wish...) with all the wishers wanting this package
+        # tuple with one list per wish (1st wish, 2nd wish...) with all the students (wishers) wanting this package
         self.wishers = wishers
 
     def __repr__(self):
@@ -22,7 +28,22 @@ class Package:
 
 
 class Selection:
+    """
+    containing all the students and packages
+    methods can assign students to packages in three different ways:
+        1. assign_all_cleanly:
+            try to assign packages that are only wanted by a single student
+            -> no package gets taken away from a different student wanting it the same way or more
+        2. assign_all_uncleanly:
+            assign students to packages with taking packages away from students wanting it the same way
+            leaving only completely unwanted packages
+        3. assign_all_dirtily:
+            assign packages to students, including completely unwanted packages
+            no packages or student stays unassigned
+    """
     def __init__(self, students, packages):
+        if len(students) != len(packages):
+            raise ValueError("The amount of students doesn't match the amount of packages!")
         # key: student number, value: Student instance
         self.students = students
         # key: package number, value: Package instance
@@ -32,10 +53,13 @@ class Selection:
         self.amount_wishes = len(self.students[1].wishes)
 
     def __repr__(self):
-        return f"{len(self.students)} wishers with {self.amount_wishes} wishes each; " \
-               f"{len(self.assigned_students)} wishers already have a package assigned to them"
+        return f"{len(self.students)} students with {self.amount_wishes} wishes each; " \
+               f"{len(self.assigned_students)} students already have a package assigned to them"
 
     def get_unassigned_wishers(self, package_number, wish_id):
+        """
+        get all the students wanting this package (according to wish_id) that aren't assigned yet
+        """
         # get Package object
         package = self.packages[package_number]
         # get all students having this package as their wish (according to wish_id) that aren't assigned
@@ -52,11 +76,11 @@ class Selection:
     def assign_package_if_possible(self, package_number, wish_id):
         """
         recursive function (calling resolve_after_assignment)
-        see if this package is only wanted by one person (according to wish_id)
+        see if this package is only wanted by one student (according to wish_id)
         and then assign it
+
+        return True when this is a package wanted by multiple students, else False
         """
-        # get package
-        package = self.packages[package_number]
         # get unassigned wishers
         unassigned_wishers = self.get_unassigned_wishers(package_number, wish_id)
         # when only a single student wants this package, they get it
@@ -67,18 +91,17 @@ class Selection:
                 self.assign(this_student_number, package_number)
 
                 # see if that assignment resolved a problem with a more important wish
-                # <- one student less to find a package again
+                # <- one student less to find a package for
                 self.resolve_after_assignment(this_student_number, wish_id - 1)
-        # when this package is wanted by multiple people
-        elif len(package.wishers[wish_id]) > 1:
+        # when this package is wanted by multiple students
+        elif len(self.packages[package_number].wishers[wish_id]) > 1:
             return True
         return False
 
     def resolve_after_assignment(self, student_number, wish_id):
         """
-        recursive function (calling assign_package_if_possible)
-        see if that assignment resolved a problem with a more important wish
-        go through all more important wishes than the current one
+        recursive function (calling itself and assign_package_if_possible)
+        see if an assignment (student_number got assigned) resolved a problem with a more important wish (wish_id)
         """
         # when this wish doesn't exists
         if wish_id < 0:
@@ -91,29 +114,31 @@ class Selection:
             # see if it can be assigned -> resolve even more problems if possible
             self.assign_package_if_possible(package_number, wish_id)
         # do the same with the next more important wish
-        self.resolve_after_assignment(package_number, wish_id - 1)
+        self.resolve_after_assignment(student_number, wish_id - 1)
 
     def assign_packages(self, wish_id, disallowed_packages):
         """
-        assign all packages that are wanted by only one student (according to wish_id)
+        (cleanly) assign all packages that are wanted by only one student (according to wish_id)
         and check if that assignment solved a problem with a more important wish
+
+        return all the packages that are wanted by multiple students
         """
         # numbers of all packages wanted by multiple students
         highly_wanted_package = []
         for package_number in self.packages.keys():
             # don't try to assign this package if it is disallowed or already assigned
-            # todo: unclean
-            if package_number in disallowed_packages or package_number in self.assigned_students.values():
-                continue
-            # assign if possible
-            wanted_by_multiple = self.assign_package_if_possible(package_number, wish_id)
-            if wanted_by_multiple:
-                highly_wanted_package.append(package_number)
+            if package_number not in disallowed_packages and package_number not in self.assigned_students.values():
+                # assign if possible
+                is_highly_wanted = self.assign_package_if_possible(package_number, wish_id)
+                # add this package if it is highly wanted
+                if is_highly_wanted:
+                    highly_wanted_package.append(package_number)
         return highly_wanted_package
 
     def assign_all_cleanly(self):
         """
-        try to assign all packages
+        try to assign packages that are only wanted by a single student
+        -> no package gets taken away from a different student wanting it the same way or more
         """
         # these packages are wanted so much that less relevant wishes can't be used to assign it
         highly_wanted_packages = []
@@ -125,52 +150,56 @@ class Selection:
     def assign_all_uncleanly(self):
         """
         go through all wishes and just assign the first student with their wished package
+        -> other students wanting this package in the same way won't get it
         """
+        # go through all unassigned students
         for wish_id in range(self.amount_wishes):
             for student_number in self.students.keys():
-                # todo: unclean
                 if student_number in self.assigned_students.keys():
+                    # this student is already assigned
                     continue
                 wished_package = self.students[student_number].wishes[wish_id]
-                # just assign if the package is not assigned yet
+                # just assign it if the package is not assigned yet
                 if wished_package not in self.assigned_students.values():
                     self.assign(student_number, wished_package)
 
     def assign_all_dirtily(self):
         """
-        go through all unassigned students and unassigned packages and just assign with noo regard to the wishes
+        go through all unassigned students and unassigned packages and just assign with no regard to their wishes
+        should only be used when there are only unwanted packages left to be assigned
         """
         # get all unassigned packages
         unassigned_package_numbers = [package_number for package_number in self.packages.keys()
                                       if package_number not in self.assigned_students.values()]
         for wish_id in range(self.amount_wishes):
             for student_number in self.students.keys():
-                # todo: unclean
-                if student_number in self.assigned_students.keys():
-                    continue
-                # just take and delete the last unassigned package and assign it
-                self.assign(student_number, unassigned_package_numbers.pop())
+                if student_number not in self.assigned_students.keys():
+                    # just take and delete the last unassigned package number and assign it
+                    self.assign(student_number, unassigned_package_numbers.pop())
 
 
 def load_file(filepath):
     with open(filepath, "r", encoding="utf-8") as file:
         lines = [line.strip() for line in file]
-        # the first line only contains the amount of wishers, which won't be used
+        # the first line only contains the amount of students, which won't be used
         return lines[1:]
 
 
 def load_students_and_packages(lines):
+    """
+    get lines read from the file and create a Selection object
+    """
     # get the amount of wishes every student has
     wishes_amount = len(re.split(r"[\W]+", lines[0]))
     # key: package number, value: Package instance
-    # fill it with no wishers wanting any packages
+    # fill it with no student wanting any packages
     packages = {number: Package(number, tuple([] for _ in range(wishes_amount))) for number in range(1, len(lines) + 1)}
 
     # key: student number, value: Student instance
     students = {}
     for student_idx, line in enumerate(lines):
         """
-        fill wishers
+        fill students
         """
         # split line into words
         wishes = tuple(int(package_number) for package_number in re.split(r"[\W]+", line))
@@ -189,7 +218,7 @@ def load_students_and_packages(lines):
 
 def main():
     # load students and packages form file
-    lines = load_file("beispieldaten/wichteln7.txt")
+    lines = load_file("beispieldaten/wichteln6.txt")
     selection = load_students_and_packages(lines)
 
     # assign all packages and students
