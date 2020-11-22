06ff88caa8c9ee71691725fc5d68fc59775aa252
Christopher Besch <christopher.besch@gmx.de>
2020-11-22 09:55:19 +0100

    task_3 smaall improvements

--- task_3/main.py
+++ task_3/main.py
@@ -12,13 +12,13 @@ def main():
     # first sys argument is needed for the filepath of the skill levels
     skill_levels = read_skill_levels(sys.argv[1])
     # second sys argument is needed for the number of repetitions
-    n= int(sys.argv[2])
+    n = int(sys.argv[2])
     players = list(range(len(skill_levels)))
     # declare counters for every win in game
     count_wins_liga = 0
     count_wins_KO = 0
     count_wins_KO5 = 0
-    repetition =n
+    repetition = n
     while repetition > 0:
         repetition -= 1
         # mix the skill levels randomly
@@ -34,9 +34,9 @@ def main():
     best_game = ""
     # compare the value of counters
     print("Wie oft hat der spielstärkste Spieler im Durschnitt gewonnen: ")
-    print("LIGA :",count_wins_liga/n)
-    print("KO   :",count_wins_KO/n)
-    print("KO5  :",count_wins_KO5/n)
+    print(f"LIGA:\t{count_wins_liga / n}")
+    print(f"KO:\t{count_wins_KO / n}")
+    print(f"KO5:\t{count_wins_KO5 / n}")
     if count_wins_liga > count_wins_KO and count_wins_liga > count_wins_KO5:
         best_game = "LIGA"
     elif count_wins_KO > count_wins_KO5 and count_wins_KO > count_wins_liga:
@@ -44,7 +44,7 @@ def main():
     else:
         best_game = "KO5"
     # print the best game mode
-    print("Beste Spielvariante: " + best_game)
+    print(f"Beste Spielvariante: {best_game}")
 
 
 if __name__ == '__main__':
